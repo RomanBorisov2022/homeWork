7b7484ccf2f1aa1e2ddbc4a598fa4ad05bfd03b2
Roman Borisov <boriso-rom@yandex.ru>
2023-04-10 19:15:19 +0300

    last commit 10 april 2023year

--- src/Main.java
+++ src/Main.java
@@ -55,6 +55,7 @@ public class Main {
         }
         System.out.println();
         service.sortById();
+
         for (Human human: iterablePeople) {
             System.out.println(human);
         }
